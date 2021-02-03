c12cf0743648e39dbdbece32cc7fdb665accc34d
YunHoseon <hosun2776@gmail.com>
2021-02-03 16:17:31 +0900

    git 설정

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 3.0f;
+    [SerializeField]
+    private float jumpPower = 10.0f;
+
+    private Rigidbody rigidbody;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Run(h, v);
+        Jump();
+    }
+
+    void Run(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+
+    }
+}
