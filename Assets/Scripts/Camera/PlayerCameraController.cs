82db035ab6936863884dc81e3618ba9e7585a434
YunHoseon <hosun2776@gmail.com>
2021-02-04 00:00:35 +0900

    0203 플레이어 이동, 애니메이션, 카메라 무브

---
+++ Assets/Scripts/Camera/PlayerCameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    private float offsetX = 0;
+    private float offsetY = 0.6f;
+    private float offsetZ = -3;
+
+    [SerializeField]
+    private float followSpeed = 3;
+
+    Vector3 cameraPosition;
+
+    void LateUpdate()
+    {
+        cameraPosition.x = target.transform.position.x + offsetX;
+        cameraPosition.y = target.transform.position.y + offsetY;
+        cameraPosition.z = target.transform.position.z + offsetZ;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,35 +5,72 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 3.0f;
+    private float moveSpeed = 3.0f;
     [SerializeField]
     private float jumpPower = 10.0f;
+    [SerializeField]
+    private float rotateSpeed = 5.0f;
+
+    private bool isJumping = false;
+    private float horizontalMove;
+    private float verticalMove;
 
     private Rigidbody rigidbody;
+    private Animator animator;
     private Vector3 movement;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        if (horizontalMove == 0 && verticalMove == 0)
+            animator.SetBool("Walk", false);
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        Run(h, v);
+        Walk(horizontalMove, verticalMove);
         Jump();
+        Rotate();
     }
 
-    void Run(float h, float v)
+    void Walk(float h, float v)
     {
+        animator.SetBool("Walk", true);
         movement.Set(h, 0, v);
-        movement = movement.normalized * speed * Time.deltaTime;
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        rigidbody.MovePosition(transform.position + movement);
     }
 
-    void Jump()
+    void Jump() //한번만 되게 해야함
     {
+        if(isJumping == false)
+            return;
 
+        animator.SetTrigger("Jump");
+        rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        isJumping = false;
+    }
+
+    void Rotate()
+    {
+        if (horizontalMove == 0 && verticalMove == 0)
+            return;
+
+        Quaternion newRotation = Quaternion.LookRotation(movement);
+        rigidbody.rotation = Quaternion.Slerp(rigidbody.rotation, newRotation, rotateSpeed * Time.deltaTime);
     }
 }
